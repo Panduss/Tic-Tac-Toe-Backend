23268f2595b82394724bd3fe25420d76d1c7576d
Zsuzsi Hudacsko <zsuzsi.hudacsko@gmail.com>
2018-05-26 08:07:23 +0200

    project boilerplate set up

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'reflect-metadata'
+import {createKoaServer} from "routing-controllers"
+import Controller from "./controller"
+
+const port = process.env.PORT || 4000
+
+const app = createKoaServer({
+   controllers: [Controller]
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+const codaisseur: string = 'Welcome Bram! Shall we play a little game?'
+console.log(codaisseur)
+
